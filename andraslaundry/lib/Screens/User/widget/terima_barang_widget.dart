61385df22ffcd8e5958f0ce460e89d90edcc532d
septiningrumputri <127274639+septiningrumputri@users.noreply.github.com>
2024-06-05 22:24:26 +0700

    Update terima_barang_widget.dart

--- andraslaundry/lib/Screens/User/widget/terima_barang_widget.dart
+++ andraslaundry/lib/Screens/User/widget/terima_barang_widget.dart
@@ -1,19 +1,102 @@
 import 'package:flutter/material.dart';
 
 class TerimaBarang extends StatelessWidget {
-  const TerimaBarang({super.key});
+  TerimaBarang({super.key});
+
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text(
-            'Halaman Terima Barang',
-            style: TextStyle(fontSize: 30),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          padding: EdgeInsets.symmetric(vertical: 65),
+          height: 800,
+          decoration: BoxDecoration(color: Colors.white),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: SearchAnchor(
+                  builder: (BuildContext context, SearchController controller) {
+                    return SearchBar(
+                      controller: controller,
+                      padding: MaterialStatePropertyAll<EdgeInsets>(
+                        EdgeInsets.symmetric(horizontal: 16.0),
+                      ),
+                      leading: Icon(Icons.search),
+                    );
+                  },
+                  suggestionsBuilder:
+                      (BuildContext context, SearchController controller) {
+                    return List<ListTile>.generate(5, (int index) {
+                      final String item = 'item $index';
+                      return ListTile(
+                        title: Text(item),
+                        onTap: () {},
+                      );
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  itemCount: entries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Card(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 30),
+                          child: Row(
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 6),
+                                  child: Icon(
+                                    Icons.person,
+                                    size: 30,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: MediaQuery.sizeOf(context).width / 1.4,
+                                child: Text(
+                                  "Tugas Bombe",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 6),
+                                  child: Icon(
+                                    Icons.menu,
+                                    size: 30,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
